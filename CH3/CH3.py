c08be96a7756bdc663a466bccf2dfae328e9dd0f
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-07-09 19:15:20 -0600

    Used logistic regression with sklearn

--- CH3/CH3.py
+++ CH3/CH3.py
@@ -188,3 +188,23 @@ plt.ylabel('Petal width [standardized]')
 plt.legend(loc = 'best')
 plt.tight_layout()
 plt.show()
+
+#Ending here on page 69
+
+
+#Start on page 70
+from sklearn.linear_model import LogisticRegression
+
+lr = LogisticRegression(C=100.0, solver= 'lbfgs', multi_class='ovr')
+lr.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std,
+                      y_combined,
+                      classifier=lr,
+                      test_idx=range(105,150))
+
+plt.xlabel('Petal length [standardized]')
+plt.ylabel('Petal width [standardized]')
+plt.legend(loc = 'upper left')
+plt.tight_layout()
+plt.show()
