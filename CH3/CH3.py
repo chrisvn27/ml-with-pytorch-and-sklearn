5751f79b558a4625c753c0232d3cd2a66c697414
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-07-12 22:46:22 -0600

    Slow progress today and decided to keep everything on a single script. I stopped at page 83

--- CH3/CH3.py
+++ CH3/CH3.py
@@ -241,4 +241,43 @@ plt.tight_layout()
 plt.show()
 
 # Finished on page 80
+#Starting on page 80
+#An example of linearly inseparable data is the XOR problem
 
+#XOR implementation
+import matplotlib.pyplot as plt
+import numpy as np
+np.random.seed(1)
+X_xor = np.random.randn(200,2)
+y_xor = np.logical_xor(X_xor[:,0] > 0,
+                       X_xor[:,1] > 0)
+y_xor = np.where(y_xor, 1, 0)
+plt.scatter(X_xor[y_xor == 1,0],
+            X_xor[y_xor == 1,1],
+            c='royalblue', marker='s',
+            label='Class 1')
+plt.scatter(X_xor[y_xor == 0,0],
+            X_xor[y_xor == 0,1],
+            c='tomato', marker='o',
+            label='Class 0')
+plt.xlim([-3,3])
+plt.ylim([-3,3])
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.legend(loc = 'best')
+plt.tight_layout()
+plt.show()
+
+#Stopping at end of page 82.
+
+#Starting end of page 82
+#Kernels for SVM are computationally expensive
+#and can be slow to train, especially with large datasets.
+#However, we can avoid this with the kernel trick. (using the kernel function)
+from sklearn.svm import SVC
+svm = SVC(kernel = 'rbf',random_state =1, gamma = 0.10, C =10.0)
+svm.fit(X_xor, y_xor)
+plot_decision_regions(X_xor, y_xor, classifier=svm)
+plt.legend(loc ='upper left')
+plt.tight_layout()
+plt.show()
