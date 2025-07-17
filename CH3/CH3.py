89ff73a76fed8b98a707345fa8bb5a5708b91641
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-07-16 22:40:37 -0600

    Understading entropy

--- CH3/CH3.py
+++ CH3/CH3.py
@@ -302,4 +302,14 @@ plt.ylabel('Petal width [standardized]')
 plt.legend(loc ='upper left')
 plt.tight_layout()
 plt.show()
-#Stopping at end of page 86 (Decision Tree learning)
+#Stopping at end of page 88 (Begining)
+
+def entropy(p):
+    return -p * np.log2(p) - (1 - p) * np.log2((1-p))
+
+x = np.arange(0.0, 1.0, 0.01)
+ent = [entropy(p) if p != 0 else None for p in x]
+plt.ylabel('Entropy')
+plt.xlabel('Class-membership probability p(i=1)')
+plt.plot(x, ent)
+plt.show()
