f51f38d5d277cc23fad7f2b14944c73ec1949105
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-08-12 14:42:40 -0600

    Run example of tree decision

--- CH3/CH3.py
+++ CH3/CH3.py
@@ -313,3 +313,18 @@ plt.ylabel('Entropy')
 plt.xlabel('Class-membership probability p(i=1)')
 plt.plot(x, ent)
 plt.show()
+
+#Building a decision tree
+from sklearn.tree import DecisionTreeClassifier
+tree_model = DecisionTreeClassifier(criterion='gini', max_depth=4, random_state=1)
+tree_model.fit(X_train, y_train)
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X_combined, y_combined, classifier = tree_model, test_idx=range(105,150))
+plt.xlabel('Petal length [cm]')
+plt.ylabel('Petal width [cm]')
+plt.legend(loc = 'upper left')
+plt.tight_layout()
+plt.show()
+
+#stopped at page 95
