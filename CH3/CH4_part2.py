69ccc9d205732b195ef58f4e8278bd7dbe491003
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-08-17 22:24:58 -0600

    Second day of CH4 further progress

--- CH3/CH4_part2.py
+++ CH3/CH4_part2.py
@@ -43,3 +43,75 @@ print(y)
 print(class_le.inverse_transform(y))
 
 #Stopped at page 113
+
+#Performing one-hot encoding on nominal features
+
+X = df[['color', 'size', 'price']].values
+color_le = LabelEncoder()
+X[:,0] = color_le.fit_transform(X[:,0])
+print(X)
+
+#Implementing one-hot encoding with sklearn's OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder
+X = df[['color', 'size', 'price']].values
+color_ohe = OneHotEncoder()
+print(color_ohe.fit_transform(X[:,0].reshape(-1,1)).toarray())
+
+from sklearn.compose import ColumnTransformer
+X = df[['color','size','price']].values
+c_transf = ColumnTransformer([
+    ('onehot', OneHotEncoder(), [0]),
+    ('nothing', 'passthrough', [1,2])
+])
+print(c_transf.fit_transform(X).astype(float))
+
+#Even more simple is using pandas get_dummies
+print(pd.get_dummies(df[['price','color','size']]))
+
+#Dropping one dummy variable to avoid the dummy variable trap
+print(pd.get_dummies(df[['price','color','size']],drop_first=True))
+
+#Doing the same with OneHotEncoder
+color_ohe = OneHotEncoder(categories='auto', drop= 'first')
+c_transf = ColumnTransformer([
+    ('onehot', color_ohe, [0]),
+    ('nothing', 'passthrough', [1,2])
+])
+
+print(c_transf.fit_transform(X).astype(float))
+
+# Encoding odrinal features
+df = pd.DataFrame([
+    ['green', 'M', 10.1, 'class2'],
+    ['red', 'L', 13.5, 'class1'],
+    ['blue', 'XL', 15.3, 'class2']
+])
+
+df.columns = ['color', 'size', 'price', 'classlabel']
+
+print(df)
+
+df['x > M'] = df['size'].apply( lambda x: 1 if x in {'L', 'XL'} else 0)
+df['x > L'] = df['size'].apply( lambda x: 1 if x == 'XL' else 0)
+
+del df['size']
+print(df)
+
+#partitioning data into training and test sets
+df_wine = pd.read_csv(
+'https://archive.ics.uci.edu/ml/'
+'machine-learning-databases/wine/wine.data',
+header=None
+)
+
+df_wine.columns = ['Class label', 'Alcohol',
+              'Malic acid', 'Ash',
+              'Alcalinity of ash', 'Magnesium',
+              'Total phenols', 'Flavanoids',
+              'Nonflavanoid phenols', 'Proanthocyanins',
+              'Color intensity', 'Hue',
+              'OD280/0D315 of diluted wines',
+              'Proline']
+
+print('Class labels', np.unique(df_wine['Class label']))
+print(df_wine.head())
