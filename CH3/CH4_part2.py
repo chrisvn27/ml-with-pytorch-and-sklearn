3c3cb57427e776211a6219233853e73fb6f9fb12
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-08-18 16:29:09 -0600

    Developed a sequential backward selection (SBS)

--- CH3/CH4_part2.py
+++ CH3/CH4_part2.py
@@ -115,3 +115,156 @@ df_wine.columns = ['Class label', 'Alcohol',
 
 print('Class labels', np.unique(df_wine['Class label']))
 print(df_wine.head())
+
+#spltting the dataset 
+from sklearn.model_selection import train_test_split
+X, y = df_wine.iloc[:,1:].values, df_wine.iloc[:,0].values
+X_train, X_test, y_train, y_test =\
+      train_test_split(X, y, test_size=0.3, random_state=0, stratify=y)
+
+
+#Stopped at page 119
+
+#Using min-max scaling, a way to normalize features
+from sklearn.preprocessing import MinMaxScaler
+mms = MinMaxScaler()
+X_train_norm = mms.fit_transform(X_train)
+X_test_norm = mms.transform(X_test)
+
+#Standardization can be more practical for many machine learning algorithms,
+# escpecially for optimization algorithms such as gradient descent
+
+ex = np.array([0,1 ,2 ,3 ,4 ,5])
+print('standardized:', (ex - ex.mean()) / ex.std())
+
+print('normalized:', (ex - ex.min())/ (ex.max() - ex.min()))
+
+#Using sklearn
+from sklearn.preprocessing import StandardScaler
+stdsc = StandardScaler()
+X_train_std = stdsc.fit_transform(X_train)
+X_test_std = stdsc.transform(X_test)
+
+# L1 and L2 regularization
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression(penalty = 'l1', C= 1.0, solver= 'liblinear', multi_class='ovr')
+lr.fit(X_train_std, y_train)
+print('Training accuracy:', lr.score(X_train_std, y_train))
+
+print('Test accuracy:', lr.score(X_test_std, y_test))
+
+print(lr.intercept_)
+print(lr.coef_)
+
+import matplotlib.pyplot as plt
+fit = plt.figure()
+ax = plt.subplot(111)
+colors = ['blue', 'green', 'red', 'cyan',
+          'magenta', 'yellow', 'black',
+          'pink', 'lightgreen', 'lightblue',
+          'gray', 'indigo', 'orange']
+
+weights , params = [],[]
+for c in np.arange(-4.,6.0):
+    lr = LogisticRegression(penalty='l1', C=10.**c,
+                            solver='liblinear', multi_class='ovr', random_state=0)
+
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10**c)
+
+weights = np.array(weights)
+for column, color in zip(range(weights.shape[1]),colors):
+    plt.plot(params, weights[:, column],
+             label = df_wine.columns[column+1],
+             color= color)
+plt.axhline(0, color='black', linestyle='--', linewidth=3)
+plt.xlim([10**(-5), 10**5])
+plt.ylabel('Weight coefficient')
+plt.xlabel('C (inverse regularization strength)')
+plt.xscale('log')
+plt.legend(loc = 'upper left')
+ax.legend(loc='upper center',
+          bbox_to_anchor=(1.38, 1.03),
+          ncol=1, fancybox=True)
+plt.show()
+
+from sklearn.base import clone
+from itertools import combinations
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+#Implementing sequential backward selection (SBS)
+
+class SBS():
+    def __init__(self, estimator, k_features,
+                 scoring = accuracy_score,
+                 test_size = 0.25, random_state =1):
+        self.scoring = scoring
+        self.estimator = clone(estimator)
+        self.k_features = k_features
+        self.test_size = test_size
+        self.random_state = random_state
+    
+    def fit(self, X, y):
+        X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size= self.test_size,
+                         random_state = self.random_state)
+        dim = X_train.shape[1]
+        self.indices_ = tuple(range(dim))
+        self.subsets_ = [self.indices_]
+        score = self._calc_score(X_train, y_train,
+                                X_test, y_test, self.indices_)
+        self.scores_ = [score]
+        while dim > self.k_features:
+            scores = []
+            subsets = []
+
+            for p in combinations(self.indices_, r= dim-1):
+                score = self._calc_score(X_train, y_train, X_test, y_test, p)
+                scores.append(score)
+                subsets.append(p)
+            
+            best = np.argmax(scores)
+            self.indices_ = subsets[best]
+            self.subsets_.append(self.indices_)
+            dim -= 1
+
+            self.scores_.append(scores[best])
+        
+        self.k_score_ = self.scores_[-1]
+
+        return self
+    
+    def transform(self, X):
+        return X[:, self.indices_]
+    
+    def _calc_score(self, X_train, y_train, X_test, y_test, indices):
+        self.estimator.fit(X_train[:, indices], y_train)
+        y_pred = self.estimator.predict(X_test[:, indices])
+        score = self.scoring(y_test, y_pred)
+        return score
+    
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors=5)
+sbs = SBS(knn, k_features= 1)
+sbs.fit(X_train_std, y_train)
+
+k_feat = [len(k) for k in sbs.subsets_]
+plt.plot(k_feat, sbs.scores_, marker='o')
+plt.ylim([0.7, 1.02])
+plt.ylabel('Accuracy')
+plt.xlabel('Number of features')
+plt.grid()
+plt.tight_layout()
+plt.show()
+
+k3 = list(sbs.subsets_[10])
+print(df_wine.columns[1:][k3])
+
+knn.fit(X_train_std, y_train)
+print('Training accuracy:', knn.score(X_train_std, y_train))
+print('Test accuracy:', knn.score(X_test_std, y_test))
