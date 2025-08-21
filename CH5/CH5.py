f5b4f75f7593053c7b0d9fc4b2b3cbbbb7073407
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-08-20 18:59:56 -0600

    Further understandin in Principal Component Analysis (PCA)

--- CH5/CH5.py
+++ CH5/CH5.py
@@ -5,6 +5,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import numpy as np
+import matplotlib.pyplot as plt
 
 df_wine = pd.read_csv(
 'https://archive.ics.uci.edu/ml/'
@@ -21,6 +22,52 @@ sc = StandardScaler()
 X_train_std = sc.fit_transform(X_train)
 X_test_std = sc.transform(X_test)
 
-cov_mat = np.cov(X_train_std)
+cov_mat = np.cov(X_train_std.T)
 eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
 print('\nEigenvalues \n', eigen_vals)
+#print('\nEigenvectors \n', eigen_vecs)
+
+#Stopped at page 144
+
+# Variance ratio = lambda_j / Sumj=1_to_d(lambda j)
+
+tot = sum(eigen_vals)
+var_exp = [(i / tot) for i in sorted(eigen_vals, reverse= True)]
+cum_var_exp = np.cumsum(var_exp)
+
+plt.bar(range(1,14), var_exp, align='center', label = 'Individual explained variance')
+plt.step(range(1,14), cum_var_exp, where='mid', label = 'Cumulative explained variance')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Principal component index')
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
+
+#Feture Transformation
+#Make a list of (eigenvalue, eigenvector) tuples
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:,i]) for i in range(len(eigen_vals))]
+
+# Sort the (eigenvalue, eigenvector) tuples from high to low
+eigen_pairs.sort(key= lambda k: k[0], reverse= True) #k[0] is sorting about the first input of the tupple : eigenvalue
+
+W = np.hstack((eigen_pairs[0][1][:, np.newaxis], eigen_pairs[1][1][:, np.newaxis]))
+
+print('Matrix W: \n', W)
+
+#Reducing dimentionality: x' = xW (x' = 1x2, x=1x13, W=13x2)
+X_train_pca = X_train_std.dot(W)
+
+colors = ['r', 'b', 'g']
+markers = ['o', 's', '^']
+
+for l, c, m in zip(np.unique(y_train), colors, markers):
+    plt.scatter(X_train_pca[y_train==l, 0],
+                X_train_pca[y_train==l, 1],
+                c=c, label = f'Class {l}', marker = m)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc = 'lower left')
+plt.tight_layout()
+plt.show()
+
+#Stopped at page 149
