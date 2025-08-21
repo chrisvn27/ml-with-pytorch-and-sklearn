361889e8a6418c2941d564fe18aac232e9133710
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-08-21 17:06:23 -0600

    Finished PCA

--- CH5/CH5.py
+++ CH5/CH5.py
@@ -71,3 +71,93 @@ plt.tight_layout()
 plt.show()
 
 #Stopped at page 149
+#Using scikit learn
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, test_idx= None, resolution =0.02):
+
+    # setup marker generator and color map
+    markers = ('o', 's', '^', 'v', '<')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    #plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class examples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.8,
+                    c = colors[idx],
+                    marker=markers[idx],
+                    label=f"Class {cl}",
+                    edgecolor='black')
+        
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+# Initializing the PCA transformer and logistic regression estimator:
+pca = PCA(n_components=2)
+lr = LogisticRegression(multi_class='ovr',
+                        random_state=1,
+                        solver='lbfgs')
+# dimensionality reduction:
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+
+# fitting the logistic regression model on the reduced dataset:
+lr.fit(X_train_pca, y_train)
+plot_decision_regions(X_train_pca, y_train, classifier= lr)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc = 'lower left')
+plt.tight_layout()
+plt.show()
+
+plot_decision_regions(X_test_pca, y_test, classifier=lr)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc = 'lower left')
+plt.tight_layout()
+plt.show()
+
+pca = PCA(n_components=None)
+X_train_pca = pca.fit_transform(X_train_std)
+print(pca.explained_variance_)
+
+#Assessing feature contributions
+
+print("eigen_vecs shape:", eigen_vecs.shape)
+print("eigen_values shape:", eigen_vals.shape)
+
+loadings = eigen_vecs * np.sqrt(eigen_vals)
+
+print("loadings shape: ", loadings.shape)
+
+fig, ax = plt.subplots()
+ax.bar( range(13), loadings[:, 0], align='center')
+ax.set_ylabel("Loadings for PC1")
+ax.set_xticks(range(13))
+ax.set_xticklabels(df_wine.columns[1:], rotation=90)
+plt.ylim([-1,1])
+plt.show()
+
+#Using scikit learn
+sklearn_loadings = pca.components_.T * np.sqrt(pca.explained_variance_)
+fig, ax = plt.subplots()
+ax.bar( range(13), sklearn_loadings[:, 0], align='center')
+ax.set_ylabel("Loadings for PC1")
+ax.set_xticks(range(13))
+ax.set_xticklabels(df_wine.columns[1:], rotation=90)
+plt.ylim([-1,1])
+plt.show()
+
+#Stopped in page 154
