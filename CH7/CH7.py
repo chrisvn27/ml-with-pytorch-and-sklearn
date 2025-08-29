dab02f1190791a52ab97bad3dc3910a567e38004
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-08-29 00:04:31 -0600

    The class I implemented was outdated and I needed to check onto the github's authors to find updates. I was able to find the update and tomorrow I'll check if there are any other differences.

--- CH7/CH7.py
+++ CH7/CH7.py
@@ -45,70 +45,203 @@ from sklearn.base import clone
 from sklearn.pipeline import _name_estimators
 import operator
 
-class MajorityVoteClassifier(BaseEstimator, ClassifierMixin):
-    def __init__(self, classifiers, vote='classlabel', weights=None):
+# class MajorityVoteClassifier(BaseEstimator, ClassifierMixin):
+#     def __init__(self, classifiers, vote='classlabel', weights=None):
         
+#         self.classifiers = classifiers
+#         self.named_classifiers ={
+#             key: value for key, value in _name_estimators(classifiers)
+#         }
+#         self.vote = vote
+#         self.weights= weights
+
+#     def fit(self, X, y):
+#         if self.vote not in ('probability', 'classlabel'):
+#             raise ValueError(f"vote must be 'probability' "
+#                              f" or 'classlabel'"
+#                              f"; got (vote={self.vote})")
+#         if self.weights and len(self.weights) != len(self.classifiers):
+#             raise ValueError(f"Number of classifiers and"
+#                              f" weights must be equal"
+#                              f"; got {len(self.weights)} weights"
+#                              f" {len(self.classifiers)} classifiers")
+#         # Use LabelEncoder to ensure class labels start
+#         # with 0, which is important for np.argmax
+#         # class in self.predict
+#         self.lablenc_ = LabelEncoder()
+#         self.lablenc_.fit(y)
+#         self.classes_ = self.lablenc_.classes_
+#         self.classifiers_ = []
+        
+#         for clf in self.classifiers:
+#             fitted_clf = clone(clf).fit(X, self.lablenc_.transform(y))
+#             self.classifiers_.append(fitted_clf)
+#         return self
+    
+#     def predict(self, X):
+#         if self.vote == 'probability':
+#             maj_vote = np.argmax(self.predict_proba(X), axis=1)
+#         else: # 'class label vote'
+#             predictions = np.asarray([
+#                 clf.predict(X) for clf in self.classifiers_
+#             ]).T
+        
+#         maj_vote = np.apply_along_axis(lambda x: np.argmax(
+#             np.bincount(x, weights= self.weights)
+#         ),
+#         axis=1, arr=predictions
+#         )
+#         maj_vote = self.lablenc_.inverse_transform(maj_vote)
+#         return maj_vote
+    
+#     def predict_proba(self, X):
+#         probas = np.asarray([clf.predict_proba(X)
+#                              for clf in self.classifiers_])
+#         avg_proba = np.average(probas, axis=0, weights= self.weights)
+#         return avg_proba
+#     def get_params(self, deep = True):
+#         if not deep:
+#             return super().get_params(deep=False)
+#         else:
+#             out = self.named_classifiers.copy()
+#             for name, step in self.named_classifiers.items():
+#                 for key,value in step.get_params(
+#                     deep=True).items():
+#                     out[f'{name}__{key}'] = value
+#             return out
+
+
+# Scikit-learn 0.16 and newer requires reversing the parent classes
+# See https://github.com/rasbt/machine-learning-book/discussions/205 for more details
+import sklearn
+base_classes = (ClassifierMixin, BaseEstimator) if sklearn.__version__ >= "0.16" else (BaseEstimator, ClassifierMixin)
+
+class MajorityVoteClassifier(*base_classes):
+    """ A majority vote ensemble classifier
+
+    Parameters
+    ----------
+    classifiers : array-like, shape = [n_classifiers]
+      Different classifiers for the ensemble
+
+    vote : str, {'classlabel', 'probability'} (default='classlabel')
+      If 'classlabel' the prediction is based on the argmax of
+        class labels. Else if 'probability', the argmax of
+        the sum of probabilities is used to predict the class label
+        (recommended for calibrated classifiers).
+
+    weights : array-like, shape = [n_classifiers], optional (default=None)
+      If a list of `int` or `float` values are provided, the classifiers
+      are weighted by importance; Uses uniform weights if `weights=None`.
+
+    """
+    def __init__(self, classifiers, vote='classlabel', weights=None):
+
         self.classifiers = classifiers
-        self.named_classifiers ={
-            key: value for key, value in _name_estimators(classifiers)
-        }
+        self.named_classifiers = {key: value for key, value
+                                  in _name_estimators(classifiers)}
         self.vote = vote
-        self.weights= weights
+        self.weights = weights
 
     def fit(self, X, y):
+        """ Fit classifiers.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape = [n_examples, n_features]
+            Matrix of training examples.
+
+        y : array-like, shape = [n_examples]
+            Vector of target class labels.
+
+        Returns
+        -------
+        self : object
+
+        """
         if self.vote not in ('probability', 'classlabel'):
-            raise ValueError(f"vote must be 'probability' "
-                             f" or 'classlabel'"
+            raise ValueError(f"vote must be 'probability' or 'classlabel'"
                              f"; got (vote={self.vote})")
+
         if self.weights and len(self.weights) != len(self.classifiers):
-            raise ValueError(f"Number of classifiers and"
-                             f" weights must be equal"
-                             f"; got {len(self.weights)} weights"
-                             f" {len(self.classifiers)} classifiers")
-        # Use LabelEncoder to ensure class labels start
-        # with 0, which is important for np.argmax
-        # class in self.predict
+            raise ValueError(f'Number of classifiers and weights must be equal'
+                             f'; got {len(self.weights)} weights,'
+                             f' {len(self.classifiers)} classifiers')
+
+        # Use LabelEncoder to ensure class labels start with 0, which
+        # is important for np.argmax call in self.predict
         self.lablenc_ = LabelEncoder()
         self.lablenc_.fit(y)
         self.classes_ = self.lablenc_.classes_
         self.classifiers_ = []
-        
         for clf in self.classifiers:
             fitted_clf = clone(clf).fit(X, self.lablenc_.transform(y))
             self.classifiers_.append(fitted_clf)
         return self
-    
+
     def predict(self, X):
+        """ Predict class labels for X.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape = [n_examples, n_features]
+            Matrix of training examples.
+
+        Returns
+        ----------
+        maj_vote : array-like, shape = [n_examples]
+            Predicted class labels.
+            
+        """
         if self.vote == 'probability':
-            maj_vote = np.argmax(self.predict(X), axis=1)
-        else: # 'class label vote'
-            predictions = np.asarray([
-                clf.predic(X) for clf in self.classifiers_
-            ]).T
-        
-        maj_vote = np.apply_along_axis(lambda x: np.argmax(
-            np.bincount(x, weights= self.weights)
-        ),
-        axis=1, arr=predictions
-        )
+            maj_vote = np.argmax(self.predict_proba(X), axis=1)
+        else:  # 'classlabel' vote
+
+            #  Collect results from clf.predict calls
+            predictions = np.asarray([clf.predict(X)
+                                      for clf in self.classifiers_]).T
+
+            maj_vote = np.apply_along_axis(
+                                      lambda x:
+                                      np.argmax(np.bincount(x,
+                                                weights=self.weights)),
+                                      axis=1,
+                                      arr=predictions)
         maj_vote = self.lablenc_.inverse_transform(maj_vote)
         return maj_vote
-    
+
     def predict_proba(self, X):
-        probas = np.asarray([clf.predic_proba(X)
+        """ Predict class probabilities for X.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape = [n_examples, n_features]
+            Training vectors, where n_examples is the number of examples and
+            n_features is the number of features.
+
+        Returns
+        ----------
+        avg_proba : array-like, shape = [n_examples, n_classes]
+            Weighted average probability for each class per example.
+
+        """
+        probas = np.asarray([clf.predict_proba(X)
                              for clf in self.classifiers_])
-        avg_proba = np.average(probas, axis=0, weights= self.weights)
+        avg_proba = np.average(probas, axis=0, weights=self.weights)
         return avg_proba
-    def get_params(self, deep = True):
+
+    def get_params(self, deep=True):
+        """ Get classifier parameter names for GridSearch"""
         if not deep:
             return super().get_params(deep=False)
         else:
             out = self.named_classifiers.copy()
             for name, step in self.named_classifiers.items():
-                for key,value in step.get_params(
-                    deep=True).items():
-                    out[f"{name}__{key}"] = value
+                for key, value in step.get_params(deep=True).items():
+                    out[f'{name}__{key}'] = value
             return out
+
+
 
 # Using the majority voting principle to make predictions
 
@@ -154,6 +287,21 @@ for clf, label in zip([pipe1, clf2, pipe3], clf_labels):
                              cv=10,
                              scoring='roc_auc')
     
+    print(f"ROC AUC: {scores.mean():.2f} "
+          f"(+/- {scores.std():.2f}) [{label}]")
+    
+#Stopped at page 216
+mv_clf = MajorityVoteClassifier(
+    classifiers=[pipe1,clf2,pipe3]
+)
+clf_labels += ['Majority voting']
+all_clf = [pipe1, clf2, pipe3, mv_clf]
+for clf, label in zip(all_clf, clf_labels):
+    scores = cross_val_score(estimator=clf,
+                             X=X_train,
+                             y=y_train,
+                             cv=10,
+                             scoring='roc_auc')
     print(f"ROC AUC: {scores.mean():.2f} "
           f"(+/- {scores.std():.2f}) [{label}]")
     
