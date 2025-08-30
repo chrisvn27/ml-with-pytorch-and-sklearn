23d1cda513353149a9439275ec646c742884a3d3
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-08-30 17:12:33 -0600

    Further progress on CH7

--- CH7/CH7.py
+++ CH7/CH7.py
@@ -45,71 +45,6 @@ from sklearn.base import clone
 from sklearn.pipeline import _name_estimators
 import operator
 
-# class MajorityVoteClassifier(BaseEstimator, ClassifierMixin):
-#     def __init__(self, classifiers, vote='classlabel', weights=None):
-        
-#         self.classifiers = classifiers
-#         self.named_classifiers ={
-#             key: value for key, value in _name_estimators(classifiers)
-#         }
-#         self.vote = vote
-#         self.weights= weights
-
-#     def fit(self, X, y):
-#         if self.vote not in ('probability', 'classlabel'):
-#             raise ValueError(f"vote must be 'probability' "
-#                              f" or 'classlabel'"
-#                              f"; got (vote={self.vote})")
-#         if self.weights and len(self.weights) != len(self.classifiers):
-#             raise ValueError(f"Number of classifiers and"
-#                              f" weights must be equal"
-#                              f"; got {len(self.weights)} weights"
-#                              f" {len(self.classifiers)} classifiers")
-#         # Use LabelEncoder to ensure class labels start
-#         # with 0, which is important for np.argmax
-#         # class in self.predict
-#         self.lablenc_ = LabelEncoder()
-#         self.lablenc_.fit(y)
-#         self.classes_ = self.lablenc_.classes_
-#         self.classifiers_ = []
-        
-#         for clf in self.classifiers:
-#             fitted_clf = clone(clf).fit(X, self.lablenc_.transform(y))
-#             self.classifiers_.append(fitted_clf)
-#         return self
-    
-#     def predict(self, X):
-#         if self.vote == 'probability':
-#             maj_vote = np.argmax(self.predict_proba(X), axis=1)
-#         else: # 'class label vote'
-#             predictions = np.asarray([
-#                 clf.predict(X) for clf in self.classifiers_
-#             ]).T
-        
-#         maj_vote = np.apply_along_axis(lambda x: np.argmax(
-#             np.bincount(x, weights= self.weights)
-#         ),
-#         axis=1, arr=predictions
-#         )
-#         maj_vote = self.lablenc_.inverse_transform(maj_vote)
-#         return maj_vote
-    
-#     def predict_proba(self, X):
-#         probas = np.asarray([clf.predict_proba(X)
-#                              for clf in self.classifiers_])
-#         avg_proba = np.average(probas, axis=0, weights= self.weights)
-#         return avg_proba
-#     def get_params(self, deep = True):
-#         if not deep:
-#             return super().get_params(deep=False)
-#         else:
-#             out = self.named_classifiers.copy()
-#             for name, step in self.named_classifiers.items():
-#                 for key,value in step.get_params(
-#                     deep=True).items():
-#                     out[f'{name}__{key}'] = value
-#             return out
-
 
 # Scikit-learn 0.16 and newer requires reversing the parent classes
 # See https://github.com/rasbt/machine-learning-book/discussions/205 for more details
@@ -304,3 +239,104 @@ for clf, label in zip(all_clf, clf_labels):
     print(f"ROC AUC: {scores.mean():.2f} "
           f"(+/- {scores.std():.2f}) [{label}]")
     
+mv_clf = MajorityVoteClassifier(
+    classifiers=[pipe1, clf2, pipe3]
+)
+clf_labels += ['Majority voting']
+all_clf = [pipe1, clf2, pipe3, mv_clf]
+for clf, label in zip(all_clf, clf_labels):
+    scores = cross_val_score(estimator=clf,
+                             X=X_train,
+                             y=y_train,
+                             cv=10,
+                             scoring='roc_auc')
+    print(f"ROC AUC: {scores.mean():.2f}"
+          f"+/- {scores.std():.2f} [{label}]")
+    
+
+# Evaluating and tunin the ensemble classifier
+
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+colors =['black', 'orange', 'blue', 'green']
+linestyles = [':', '--', '-.', '-']
+for clf, label, clr, ls \
+    in zip(all_clf, clf_labels, colors, linestyles):
+    # assuming the label of the positive class is 1
+    y_pred = clf.fit(X_train,
+                     y_train).predict_proba(X_test)[:, 1]
+    fpr, tpr, thresholds = roc_curve(y_true=y_test, y_score=y_pred)
+    roc_auc = auc(x=fpr, y=tpr)
+    plt.plot(fpr, tpr,
+             color= clr,
+             linestyle = ls,
+             label = f"{label} (auc = {roc_auc:.2f})")
+plt.legend(loc='lower right')
+plt.plot([0,1], [0,1],
+         linestyle= '--',
+         color='gray',
+         linewidth=2)
+plt.xlim([-0.1, 1.1])
+plt.ylim([-0.1, 1.1])
+plt.grid(alpha = 0.5)
+plt.xlabel('False positive rate (FPR)')
+plt.ylabel('True positive rate (TPR)')
+plt.show()
+
+sc = StandardScaler()
+x_train_std = sc.fit_transform(X_train)
+from itertools import product
+x_min = x_train_std[:, 0].min() - 1
+x_max = x_train_std[:, 0].max() + 1
+y_min = x_train_std[:, 1].min() - 1
+y_max = x_train_std[:, 1].max() + 1
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+                     np.arange(y_min, y_max, 0.1))
+f, axarr = plt.subplots(nrows=2, ncols=2,
+                        sharex='col', sharey='row',
+                        figsize=(7, 5))
+for idx, clf, tt in zip(product([0 , 1], [0,1]), all_clf, clf_labels):
+    clf.fit(x_train_std, y_train)
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z= Z.reshape(xx.shape)
+    axarr[idx[0], idx[1]].contourf(xx, yy, Z, alpha =0.3)
+    axarr[idx[0], idx[1]].scatter(x_train_std[y_train== 0,0],
+                                  x_train_std[y_train ==0,1],
+                                  c='blue',
+                                  marker='^',
+                                  s=50)
+    axarr[idx[0], idx[1]].scatter(x_train_std[y_train==1, 0],
+                                  x_train_std[y_train==1, 1],
+                                  c='green',
+                                  marker='o',
+                                  s=50)
+    axarr[idx[0], idx[1]].set_title(tt)
+plt.text(-3.5, -5.0,
+         s="Speal width [Standardized]",
+         ha ="center", va="center", fontsize=12)
+plt.text(-12.5, 4.5,
+         s='Petal length [standardized]',
+         ha='center', va='center',
+         fontsize=12, rotation=90)
+plt.show()
+
+print(mv_clf.get_params())
+
+from sklearn.model_selection import GridSearchCV
+params = {'decisiontreeclassifier__max_depth':[1,2],
+          'pipeline-1__clf__C': [0.001, 0.1, 100.0]}
+grid = GridSearchCV(estimator=mv_clf,
+                    param_grid= params,
+                    cv=10,
+                    scoring='roc_auc')
+grid.fit(X_train,y_train)
+
+for r, _ in enumerate(grid.cv_results_['mean_test_score']):
+    mean_score = grid.cv_results_['mean_test_score'][r]
+    std_dev = grid.cv_results_['std_test_score'][r]
+    params = grid.cv_results_['params'][r]
+    print(f"{mean_score:.3f} +/- {std_dev:.2f} {params}")
+
+print(f"Best parameters: {grid.best_params_}")
+print(f"ROC AUC: {grid.best_score_:.2f}")
