6bb203d4e3157b32868eca74f0a543837d5b49cf
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-09-02 13:39:44 -0600

    Finished CH7

--- CH7/CH7_part2.py
+++ CH7/CH7_part2.py
@@ -193,3 +193,22 @@ plt.show()
 
 
 #Stopped at page 237
+
+# Using XGBoost
+
+import xgboost as xgb
+
+model = xgb.XGBClassifier(n_estimators=1000, learning_rate=0.01,
+                          max_depth=4, random_state=1,
+                          use_label_encoder=False)
+gbm = model.fit(X_train, y_train)
+y_train_pred = gbm.predict(X_train)
+y_test_pred = gbm.predict(X_test)
+
+gbm_train = accuracy_score(y_train, y_train_pred)
+gbm_test = accuracy_score(y_test, y_test_pred)
+print(f"XGboost train/test accuracies"
+      f"{gbm_train:.3f}/{gbm_test:.3f}")
+
+
+
