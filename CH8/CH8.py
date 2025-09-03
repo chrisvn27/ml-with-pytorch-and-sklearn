c83b7c0487ad767b2c7cda5f50aded049daaa377
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-09-03 17:34:17 -0600

    Small progress in CH8

--- CH8/CH8.py
+++ CH8/CH8.py
@@ -39,3 +39,37 @@ df = pd.read_csv('movie_data.csv', encoding='utf-8')
 df = df.rename(columns={"0":"review", "1": "sentiment"})
 print(df.head(3))
 print(df.shape)
+
+#Introducing the bag-of-words model
+
+#Transforming words into feature vectors
+
+from sklearn.feature_extraction.text import CountVectorizer
+count = CountVectorizer()
+docs = np.array(["The sun is shining",
+                 "The weater is sweet",
+                 "The sun is shining, the weater is sweet,"
+                 "and one and one is two"])
+bag = count.fit_transform(docs)
+
+print(count.vocabulary_)
+print(bag.toarray())
+
+#Assessing word relevancy via term frequency-inverse document frequency
+from sklearn.feature_extraction.text import TfidfTransformer
+tfidf = TfidfTransformer(use_idf=True,
+                         norm='l2',
+                         smooth_idf=True)
+np.set_printoptions(precision=2)
+print(tfidf.fit_transform(count.fit_transform(docs)).toarray())
+
+#Cleaning text data
+
+print(df.loc[0, 'review'][-50:])
+
+import re
+def preprocessor(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
+    text = (re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-',''))
+    return text
