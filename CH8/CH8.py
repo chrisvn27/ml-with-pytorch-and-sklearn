f01e7f3c5eedc5bc96cd1723097ab4ec09f2b093
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-09-11 15:50:22 -0600

    Small progress

--- CH8/CH8.py
+++ CH8/CH8.py
@@ -111,8 +111,8 @@ y_test = df.loc[25000:, 'sentiment'].values
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
-from sklearn.feature_extraction.text import TfidfTransformer
-tfidf = TfidfTransformer(strip_accents = None,
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidf = TfidfVectorizer(strip_accents = None,
                          lowercase= False,
                          preprocessor=None)
 small_param_grid = [
@@ -120,7 +120,7 @@ small_param_grid = [
         'vect__ngram_range':[(1,1)],
         'vect__stop_words':[None],
         'vect__tokenizer': [tokenizer, tokenizer_porter],
-        'clf__pentaly':['l2'],
+        'clf__penalty':['l2'],
         'clf__C': [1.0, 10.0]
     },
     {
@@ -133,13 +133,46 @@ small_param_grid = [
         'clf__C': [1.0, 10.0]
     },
 ]
-lr_tfidf = Pipeline([
-    ('vect', tfidf),
-    ('clf', LogisticRegression(solver='liblinear'))
-])
-gs_lr_tdidf = GridSearchCV(lr_tfidf, small_param_grid,
-                           scoring='accuracy', cv=5,
-                           verbose=2, n_jobs=-1)
-gs_lr_tdidf.fit(X_train,y_train)
 
-#Stopped at beginning of page 259
+# lr_tfidf = Pipeline([
+#     ('vect', tfidf),
+#     ('clf', LogisticRegression(solver='liblinear'))
+# ])
+# gs_lr_tdidf = GridSearchCV(lr_tfidf, small_param_grid,
+#                            scoring='accuracy', cv=5,
+#                            verbose=2, n_jobs=-1)
+# gs_lr_tdidf.fit(X_train,y_train)
+
+# #Stopped at beginning of page 259
+
+# print(f"Best parameter set: {gs_lr_tdidf.best_params_}")
+
+# print(f"CV Accuracy: {gs_lr_tdidf.best_score_:.3f}")
+
+# clf= gs_lr_tdidf.best_estimator_
+# print(f"Test Accuracy: {clf.score(X_test, y_test):.3f}")
+
+import numpy as np
+import re
+from nltk.corpus import stopwords
+
+stop = stopwords.words('english')
+
+def tokenizer(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
+    text = re.sub('[\W]+', ' ', text.lower()) +        ' '.join(emoticons).replace('-', '')
+    tokenized = [w for w in text.split() if w not in stop]
+    return tokenized
+
+
+def stream_docs(path):
+    with open(path, 'r', encoding='utf-8') as csv:
+        next(csv)  # skip header
+        for line in csv:
+            text, label = line[:-3], int(line[-2])
+            yield text, label
+
+print(next(stream_docs(path='movie_data.csv')))
+
+#Stopped at beginning of page 262
