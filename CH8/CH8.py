e58e32602e9a1603f008c07ee12859cc7acbd5ff
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-10-21 19:36:19 -0600

    Small progress

--- CH8/CH8.py
+++ CH8/CH8.py
@@ -176,3 +176,40 @@ def stream_docs(path):
 print(next(stream_docs(path='movie_data.csv')))
 
 #Stopped at beginning of page 262
+
+def get_minibatch(doc_stream, size):
+    docs, y = [], []
+    try:
+        for _ in range(size):
+            text, label = next(doc_stream)
+            docs.append(text)
+            y.append(label)
+    except StopIteration:
+        return None, None
+    return docs, y
+
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.linear_model import SGDClassifier
+vect= HashingVectorizer(decode_error='ignore',
+                        n_features = 2**21,
+                        preprocessor=None,
+                        tokenizer=tokenizer)
+clf = SGDClassifier(loss='log_loss', random_state=1)
+doc_stream = stream_docs(path='movie_data.csv')
+
+import pyprind
+pbar = pyprind.ProgBar(45)
+classes = np.array([0,1])
+for _ in range(45):
+    X_train, y_train = get_minibatch(doc_stream, size=1000)
+    if not X_train:
+        break
+    X_train = vect.transform(X_train)
+    clf.partial_fit(X_train, y_train, classes= classes)
+    pbar.update()
+
+X_test , y_test = get_minibatch(doc_stream, size=5000)
+X_test = vect.transform(X_test)
+print(f"Accuracy: {clf.score(X_test, y_test):.3f}")
+
+clf = clf.partial_fit(X_test, y_test)
