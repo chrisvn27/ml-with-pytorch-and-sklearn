0be078a4dc5d21c073f965a3680405db49879325
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-10-22 18:23:56 -0600

    Finished CH8

--- CH8/CH8.py
+++ CH8/CH8.py
@@ -213,3 +213,42 @@ X_test = vect.transform(X_test)
 print(f"Accuracy: {clf.score(X_test, y_test):.3f}")
 
 clf = clf.partial_fit(X_test, y_test)
+
+#Stopped at page 264
+
+#Decomposing text documents with LDA (latent Dirichlet allocation)
+
+import pandas as pd
+df = pd.read_csv('movie_data.csv', encoding='utf-8')
+
+# the following is necessary on some computers:
+df = df.rename(columns={"0":"review", "1": "sentiment"})
+
+from sklearn.feature_extraction.text import CountVectorizer
+count = CountVectorizer(stop_words='english',
+                        max_df=.1,
+                        max_features=5000)
+X = count.fit_transform(df['review'].values)
+
+## This section takes about 5 mins to run
+from sklearn.decomposition import LatentDirichletAllocation
+lda = LatentDirichletAllocation(n_components=10,
+                                random_state= 123,
+                                learning_method='batch')
+X_topics = lda.fit_transform(X)
+
+print(lda.components_.shape)
+##
+
+n_top_words = 5
+feature_names = count.get_feature_names_out()
+
+for topic_idx, topic in enumerate(lda.components_):
+    print(f"Topic {(topic_idx +1)}:")
+    print(' '.join([feature_names[i] for i in topic.argsort()\
+                    [:-n_top_words - 1:-1]]))
+    
+horror = X_topics[:, 5].argsort()[::-1]
+for iter_idx, movie_idx in enumerate(horror[:3]):
+    print(f"\nHorror movie #{iter_idx + 1}:")
+    print(df['review'][movie_idx][:300], '...')
