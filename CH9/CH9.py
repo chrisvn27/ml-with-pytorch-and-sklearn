a680b4aa6eddc2ad762ff636e5bda07d66cd81d0
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-11-15 17:57:55 -0700

    Progress on CH9

--- CH9/CH9.py
+++ CH9/CH9.py
@@ -161,3 +161,64 @@ def mean_absolute_deviation(data):
 print(mean_absolute_deviation(y))
 
 #Stopped at page 288
+from sklearn.model_selection import train_test_split
+target = 'SalePrice'
+features = df.columns[df.columns != target]
+X= df[features].values
+y = df[target].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3, random_state=123)
+slr = LinearRegression()
+slr.fit(X_train, y_train)
+y_train_pred = slr.predict(X_train)
+y_test_pred = slr.predict(X_test)
+
+x_max = np.max(
+    [np.max(y_train_pred), np.max(y_test_pred)]
+)
+x_min = np.min(
+    [np.min(y_train_pred), np.min(y_test_pred)]
+)
+
+fig, (ax1, ax2) = plt.subplots(
+    1,2, figsize=(7,3), sharey= True
+)
+ax1.scatter(
+    y_test_pred, y_test_pred - y_test,
+    c='limegreen', marker='s',
+    edgecolor='white', label = 'Test data'
+)
+ax2.scatter(
+    y_train_pred, y_train_pred - y_train,
+    c='steelblue', marker = 'o', edgecolor='white',
+    label='Training data'
+)
+ax1.set_label('Residuals')
+
+for ax in (ax1, ax2):
+    ax.set_xlabel('Predicted values')
+    ax.legend(loc='upper left')
+    ax.hlines(y=0, xmin=x_min-100, xmax=x_max+100,\
+              color='black', lw=2)
+    
+plt.tight_layout()
+plt.show()
+
+from sklearn.metrics import mean_squared_error
+mse_train = mean_squared_error(y_train, y_train_pred)
+mse_test = mean_squared_error(y_test, y_test_pred)
+print(f"MSE train: {mse_train:.2f}")
+print(f"MSE test: {mse_test:.2f}")
+
+from sklearn.metrics import mean_absolute_error
+mae_train = mean_absolute_error(y_train, y_train_pred)
+mae_test = mean_absolute_error(y_test, y_test_pred)
+print(f"MAE train: {mae_train:.2f}")
+print(f"MAE test: {mae_test:.2f}")
+
+from sklearn.metrics import r2_score
+train_r2 = r2_score(y_train, y_train_pred)
+test_r2 = r2_score(y_test, y_test_pred)
+print(f"R^2 train: {train_r2:.3f}, test: {test_r2:.3f}")
+
+#Stopped at page 292
