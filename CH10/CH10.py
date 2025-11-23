c3e37a6110c122dc6b8a613953b1a668874d9af0
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-11-22 22:45:22 -0700

    Small progress CH10

--- CH10/CH10.py
+++ CH10/CH10.py
@@ -60,3 +60,134 @@ plt.tight_layout()
 plt.show()
 
 #Stopped at page 313
+
+print(f"Distortion: {km.inertia_:.2f}")
+
+#Elbow method
+distortions = []
+for i in range(1,11):
+    km = KMeans(n_clusters=i,
+                init='k-means++',
+                n_init=10,
+                max_iter=300,
+                random_state=0)
+    km.fit(X)
+    distortions.append(km.inertia_)
+
+plt.plot(range(1,11), distortions, marker='o')
+plt.xlabel('Number of clusters')
+plt.ylabel('Distortion')
+plt.tight_layout()
+plt.show()
+
+#Silhouette plots
+km = KMeans(n_clusters=3,
+            init='k-means++',
+            n_init=10,
+            max_iter=300,
+            tol=1e-04,
+            random_state=0)
+
+y_km = km.fit_predict(X)
+
+import numpy as np
+from matplotlib import cm
+from sklearn.metrics import silhouette_samples
+cluster_labels = np.unique(y_km)
+n_clusters = cluster_labels.shape[0]
+silhoutte_vals = silhouette_samples(X, y_km, metric='euclidean')
+y_ax_lower, y_ax_upper = 0, 0
+yticks = []
+for i, c in enumerate(cluster_labels):
+    c_silhouette_vals = silhoutte_vals[y_km == c]
+    c_silhouette_vals.sort()
+    y_ax_upper += len(c_silhouette_vals)
+    color = cm.jet(float(i)/ n_clusters)
+    plt.barh(range(y_ax_lower, y_ax_upper),
+             c_silhouette_vals,
+             height=1.0,
+             edgecolor='none',
+             color=color)
+    yticks.append((y_ax_lower + y_ax_upper) / 2.0)
+    y_ax_lower += len(c_silhouette_vals)
+
+silhouette_avg = np.mean(silhoutte_vals)
+plt.axvline(silhouette_avg,
+            color='red',
+            linestyle='--')
+plt.yticks(yticks, cluster_labels + 1)
+plt.ylabel('Cluster')
+plt.xlabel('Silhouette coefficient')
+plt.tight_layout()
+plt.show()
+
+
+#Example of a bad cluster using kmeans, k=2
+
+km = KMeans(n_clusters=2,
+            init='k-means++',
+            n_init=10,
+            max_iter=300,
+            tol=1e-04,
+            random_state=0)
+y_km = km.fit_predict(X)
+
+plt.scatter(X[y_km == 0, 0],
+            X[y_km == 0, 1],
+            s=50, c='lightgreen',
+            edgecolor='black',
+            marker='s',
+            label='Cluster 1')
+
+plt.scatter(X[y_km == 1, 0],
+            X[y_km == 1, 1],
+            s=50,
+            c='orange',
+            edgecolors= 'black',
+            marker= 'o',
+            label = 'Cluster 2')
+
+plt.scatter(km.cluster_centers_[:,0],
+            km.cluster_centers_[:,1],
+            s=250,
+            marker='*',
+            c='red',
+            label='centroids')
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.legend()
+plt.grid()
+plt.tight_layout()
+plt.show()
+
+
+cluster_labels = np.unique(y_km)
+n_clusters = cluster_labels.shape[0]
+silhoutte_vals = silhouette_samples(X, y_km, metric='euclidean')
+y_ax_lower, y_ax_upper = 0, 0
+yticks=[]
+
+for i, c in enumerate(cluster_labels):
+    c_silhouette_vals = silhoutte_vals[y_km == c]
+    c_silhouette_vals.sort()
+    y_ax_upper += len(c_silhouette_vals)
+    color = cm.jet(float(i) / n_clusters)
+    plt.barh(range(y_ax_lower, y_ax_upper),
+             c_silhouette_vals,
+             height=1.0,
+             edgecolor='none',
+             color = color)
+    yticks.append((y_ax_lower + y_ax_upper) /2.0)
+    y_ax_lower += len(c_silhouette_vals)
+
+silhouette_avg = np.mean(silhoutte_vals)
+plt.axvline(silhouette_avg, color = 'red', linestyle='--')
+plt.yticks(yticks, cluster_labels + 1)
+plt.ylabel('Cluster')
+plt.xlabel('Silhouette coefficient')
+plt.tight_layout()
+plt.show()
+
+# Stopped at page 319
+
+
