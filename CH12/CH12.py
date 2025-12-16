a87b6d02d11b6624f8600c7eba855aec8bd9490d
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-12-15 22:41:15 -0700

    CH12 small progress

--- CH12/CH12.py
+++ CH12/CH12.py
@@ -69,3 +69,49 @@ S = torch.stack([A,B], axis= 1)
 print(S)
 
 #Stopped at page 378
+from torch.utils.data import DataLoader
+t = torch.arange(6,dtype=torch.float32)
+data_loader = DataLoader(t)
+
+for item in data_loader:
+    print(item)
+
+data_loader = DataLoader(t, batch_size=3, drop_last=False)
+for i, batch in enumerate(data_loader, 1):
+    print(f"batch {i}: ", batch)
+
+for batch in data_loader:
+    print(batch)
+
+torch.manual_seed(1)
+t_x = torch.rand([4,3], dtype=torch.float32)
+t_y = torch.arange(4)
+
+from torch.utils.data import Dataset
+class JointDataset(Dataset):
+    def __init__(self,x , y):
+        super().__init__()
+        self.x = x
+        self.y = y
+    
+    def __len__(self):
+        return len(self.x)
+    
+    def __getitem__(self, idx):
+        return self.x[idx], self.y[idx]
+
+torch.manual_seed(1)    
+joint_dataset = JointDataset(t_x, t_y)
+
+for example in joint_dataset:
+    print('x: ',example[0], '  y: ', example[1])
+
+torch.manual_seed(1)
+data_loader = DataLoader(dataset=joint_dataset, batch_size=2, shuffle=True)
+for i, batch in enumerate(data_loader, 1):
+    print(f"batch {i}: x : {batch[0]}, \n y: {batch[1]}")
+
+for epoch in range(2):
+    print(f"epoch {epoch + 1}")
+    for i, batch in enumerate(data_loader,1):
+        print(f"batch {i}: x : {batch[0]}, \n y: {batch[1]}")
