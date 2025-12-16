132bf3e414c565f2574fa3682693a725b8aeb4e9
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-12-16 14:00:31 -0700

    Learned new funcitonalities with dataset

--- CH12/CH12.py
+++ CH12/CH12.py
@@ -115,3 +115,46 @@ for epoch in range(2):
     print(f"epoch {epoch + 1}")
     for i, batch in enumerate(data_loader,1):
         print(f"batch {i}: x : {batch[0]}, \n y: {batch[1]}")
+
+#Stopped at page 382
+
+import pathlib
+
+import matplotlib.pyplot as plt
+import os
+from PIL import Image
+
+import torchvision
+
+image_path = './'
+celeba_dataset = torchvision.datasets.CelebA(
+    image_path, split='train', target_type='attr', download=True
+)
+
+assert isinstance(celeba_dataset, torch.utils.data.Dataset)
+
+example = next(iter(celeba_dataset))
+print(example)
+
+from itertools import islice
+fig = plt.figure(figsize=(12,8))
+for i, (image, attributes) in islice(enumerate(celeba_dataset), 18):
+    ax = fig.add_subplot(3, 6, i+1)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.imshow(image)
+    ax.set_title(f'{attributes[31]}', size=15)
+plt.show()
+
+mnist_dataset = torchvision.datasets.MNIST(image_path, 'train', download=False)
+assert isinstance(mnist_dataset, torch.utils.data.Dataset)
+example = next(iter(mnist_dataset))
+print(example)
+
+fig = plt.figure(figsize=(15,6))
+for i, (image,label) in islice(enumerate(mnist_dataset), 10):
+    ax = fig.add_subplot(2,5,i+1)
+    ax.set_xticks([]); ax.set_yticks([])
+    ax.imshow(image, cmap='gray_r')
+    ax.set_title(f"{label}", size=15)
+plt.show()
