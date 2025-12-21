edc60d15ca938328dba532b0522c13935ff6cb84
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2025-12-20 18:37:14 -0700

    Progress on CH13: used nn.module for new layers

--- CH13/CH13.py
+++ CH13/CH13.py
@@ -180,3 +180,143 @@ plt.plot(history[3], lw=4)
 plt.legend(['Train acc.', 'Validation acc.'], fontsize=15)
 ax.set_xlabel('Epochs', size=15)
 plt.show()
+
+# Writing custom layers in Pytorch
+#Creating a custom layer that is w(x + e) + b, where e is noise
+class NoisyLinear(nn.Module):
+    def __init__(self, input_size, output_size, noise_stddev=0.1):
+        super().__init__()
+        w = torch.Tensor(input_size, output_size)
+        self.w = nn.Parameter(w) # nn.Parameter is a Tensor
+                                 # that's a module parameter
+        nn.init.xavier_uniform_(self.w)
+        b = torch.Tensor(output_size).fill_(0)
+        self.b = nn.Parameter(b)
+        self.noise_stddev = noise_stddev
+
+    def forward(self, x, training=False):
+        if training:
+            noise = torch.normal(0.0, self.noise_stddev, x.shape)
+            x_new = torch.add(x, noise)
+        else:
+            x_new = x
+        return torch.add(torch.mm(x_new, self.w), self.b)
+
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('o', 's', '^', 'v', '<')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class examples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.8, 
+                    c=colors[idx],
+                    marker=markers[idx], 
+                    label=f'Class {cl}', 
+                    edgecolor='black')
+
+
+
+
+torch.manual_seed(1)
+noisy_layer = NoisyLinear(4,2)
+x = torch.zeros((1,4))
+print(noisy_layer(x, training=True))
+print(noisy_layer(x, training=True))
+print(noisy_layer(x, training=False))
+
+#Stopped at page 427
+
+class MyNoisyModule(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.l1 = NoisyLinear(2, 4, 0.07)
+        self.a1 = nn.ReLU()
+        self.l2 = nn.Linear(4, 4)
+        self.a2 = nn.ReLU()
+        self.l3 = nn.Linear(4, 1)
+        self.a3 = nn.Sigmoid()
+    
+    def forward(self, x, training = False):
+        x = self.l1(x, training)
+        x = self.a1(x)
+        x = self.l2(x)
+        x = self.a2(x)
+        x = self.l3(x)
+        x = self.a3(x)
+        return x
+    
+    def predict(self, x):
+        x = torch.tensor(x, dtype=torch.float32)
+        pred = self.forward(x)[:,0]
+        return (pred>=0.5).float()
+
+torch.manual_seed(1)
+model = MyNoisyModule()
+print(model)
+
+loss_fn = nn.BCELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.015)
+torch.manual_seed(1)
+loss_hist_train = [0] * num_epochs
+accuracy_hist_train = [0] * num_epochs
+loss_hist_valid = [0] * num_epochs
+accuracy_hist_valid = [0] * num_epochs
+for epoch in range(num_epochs):
+    for x_batch, y_batch in train_dl:
+        pred = model(x_batch, True)[:,0]
+        loss = loss_fn(pred, y_batch)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        loss_hist_train[epoch] += loss.item()
+        is_correct = (
+            (pred>=0.5).float() == y_batch
+        ).float()
+        accuracy_hist_train[epoch] += is_correct.mean()
+    loss_hist_train[epoch] /= 100/batch_size
+    accuracy_hist_train[epoch] /= 100/batch_size
+    pred = model(x_valid)[:,0]
+    loss = loss_fn(pred, y_valid)
+    loss_hist_valid[epoch] = loss.item()
+    is_correct = ((pred>=0.5).float() == y_valid).float()
+    accuracy_hist_valid[epoch] += is_correct.mean()
+
+fig = plt.figure(figsize=(16,4))
+ax = fig.add_subplot(1,3,1)
+plt.plot(loss_hist_train, lw=4)
+plt.plot(loss_hist_valid, lw=4)
+plt.legend(['Train loss', 'Validation loss'], fontsize=15)
+ax.set_xlabel('Epochs', size=15)
+ax = fig.add_subplot(1,3,2)
+plt.plot(accuracy_hist_train, lw=4)
+plt.plot(accuracy_hist_valid, lw=4)
+plt.legend(['Train acc.', 'Validation acc.'], fontsize=15)
+ax.set_xlabel('Epochs', size=15)
+ax = fig.add_subplot(1,3,3)
+plot_decision_regions(
+    X=x_valid.numpy(),
+    y=y_valid.numpy().astype(np.integer),
+    classifier=model
+)
+ax.set_xlabel(r'$x_1$', size=15)
+ax.set_ylabel(r'$x_2$', size=15)
+ax.yaxis.set_label_coords(-0.025,1)
+plt.show()
