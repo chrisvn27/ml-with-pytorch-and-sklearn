03afcf5319cd3b43449eda89a3525a9751e04d9d
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2026-01-21 22:24:48 -0700

    Setting training data for GAN

--- CH17/CH17.py
+++ CH17/CH17.py
@@ -57,3 +57,51 @@ disc_model = make_discriminator_network(
                                         num_hidden_units= disc_hidden_size,
                                         )
 print(disc_model)
+
+import torchvision
+from torchvision import transforms
+
+image_path = './CH12/'
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=(0.5), std=(0.5)),
+])
+
+mnist_dataset = torchvision.datasets.MNIST(
+    root=image_path, train=True,
+    transform=transform, download=False
+)
+
+example, label = next(iter(mnist_dataset))
+print(f"Min: {example.min()} Max: {example.max()}")
+print(example.shape)
+
+def create_noise(batch_size, z_size, mode_z):
+    if mode_z == 'uniform':
+        input_z = torch.rand(batch_size, z_size)*2 -1
+    elif mode_z == 'normal':
+        input_z = torch.randn(batch_size, z_size)
+    return input_z
+
+from torch.utils.data import DataLoader
+#One forward pass for generator and discriminator
+batch_size = 32
+dataloader = DataLoader(mnist_dataset, batch_size, shuffle=False)
+input_real, label = next(iter(dataloader))
+input_real = input_real.view(batch_size,-1)
+torch.manual_seed(1)
+mode_z = 'uniform' # 'uniform' vs. 'normal'
+input_z = create_noise(batch_size, z_size, mode_z)
+print('input-z -- shape:', input_z.shape)
+print('input_real -- shape:', input_real.shape)
+
+g_output = gen_model(input_z)
+print('Output of G -- sahpe:', g_output.shape)
+
+d_proba_real = disc_model(input_real)
+d_proba_fake = disc_model(g_output)
+print('Disc. (real) -- shape:', d_proba_real.shape)
+print('Disc. (fake) -- shape:', d_proba_fake.shape)
+
+#605
