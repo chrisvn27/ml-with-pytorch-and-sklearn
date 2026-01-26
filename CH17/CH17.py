706182a39da4f873fb1696dd03507a5d0676fbd7
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2026-01-25 21:36:05 -0700

    Finished CGAN

--- CH17/CH17.py
+++ CH17/CH17.py
@@ -104,4 +104,159 @@ d_proba_fake = disc_model(g_output)
 print('Disc. (real) -- shape:', d_proba_real.shape)
 print('Disc. (fake) -- shape:', d_proba_fake.shape)
 
-#605
+loss_fn = nn.BCELoss()
+## Loss for the Generator
+g_labels_real = torch.ones_like(d_proba_fake)
+g_loss = loss_fn(d_proba_fake, g_labels_real)
+print(f'Generator Loss: {g_loss:.4f}')
+
+## Loss for the Discriminator
+d_labels_real = torch.ones_like(d_proba_real)
+d_labels_fake = torch.zeros_like(d_proba_fake)
+d_loss_real = loss_fn(d_proba_real, d_labels_real)
+d_loss_fake = loss_fn(d_proba_fake, d_labels_fake)
+print(f'Discriminator Losses: Real {d_loss_real:.4f} Fake {d_loss_fake:.4f}')
+
+
+device = ('cuda' if torch.cuda.is_available() else 'cpu')
+
+batch_size = 64
+torch.manual_seed(1)
+np.random.seed(1)
+mnist_dl = DataLoader(mnist_dataset, batch_size=batch_size,
+                      shuffle=True, drop_last=True)
+
+gen_model = make_generator_network(
+    input_size=z_size,
+    num_hidden_layers=gen_hidden_layers,
+    num_hidden_units=gen_hidden_size,
+    num_ouput_units=np.prod(image_size)
+).to(device)
+disc_model = make_discriminator_network(
+    input_size=np.prod(image_size),
+    num_hidden_layers=disc_hidden_layers,
+    num_hidden_units=disc_hidden_size
+).to(device)
+
+loss_fn = nn.BCELoss()
+g_optimizer = torch.optim.Adam(gen_model.parameters())
+d_optimizer = torch.optim.Adam(disc_model.parameters())
+
+## Train the Discriminator
+def d_train(x):
+    disc_model.zero_grad()
+
+    # Train discriminator with a real batch
+    batch_size = x.size(0)
+    x = x.view(batch_size, -1).to(device)
+    d_labels_real = torch.ones(batch_size, 1, device=device)
+    d_proba_real = disc_model(x)
+    d_loss_real = loss_fn(d_proba_real, d_labels_real)
+
+    # Train discriminator on a fake batch
+    input_z = create_noise(batch_size, z_size, mode_z).to(device)
+    g_output = gen_model(input_z)
+    d_proba_fake = disc_model(g_output)
+    d_labels_fake = torch.zeros(batch_size, 1, device=device)
+    d_loss_fake = loss_fn(d_proba_fake, d_labels_fake)
+
+    # gradient backprop & optimize ONLY Discriminator's parameters
+    d_loss =  d_loss_real + d_loss_fake
+    d_loss.backward()
+    d_optimizer.step()
+
+    return d_loss.data.item(), d_proba_real.detach(), d_proba_fake.detach()
+
+## Train the Generator
+def g_train(x):
+    gen_model.zero_grad()
+    batch_size = x.size(0)
+    input_z = create_noise(batch_size, z_size, mode_z).to(device)
+    g_labels_real = torch.ones(batch_size, 1, device=device)
+
+    g_output = gen_model(input_z)
+    d_proba_fake = disc_model(g_output)
+    g_loss = loss_fn(d_proba_fake, g_labels_real)
+
+    # gradient backprop & optimize ONLY Generator's parameters
+    g_loss.backward()
+    g_optimizer.step()
+
+    return g_loss.data.item()
+
+fixed_z = create_noise(batch_size, z_size, mode_z).to(device)
+
+def create_samples(g_model, input_z):
+    g_output = g_model(input_z)
+    images = torch.reshape(g_output, (batch_size, *image_size))
+    return (images+1)/2.0
+
+epoch_samples = []
+all_d_losses = []
+all_g_losses = []
+all_d_real = []
+all_d_fake = []
+num_epochs = 100
+
+for epoch in range(1, num_epochs+1):
+    d_losses, g_losses = [], []
+    d_vals_real, d_vals_fake = [], []
+    for i, (x, _) in enumerate(mnist_dl):
+        d_loss, d_proba_real, d_proba_fake = d_train(x)
+        d_losses.append(d_loss)
+        g_losses.append(g_train(x))
+        d_vals_real.append(d_proba_real.mean().cpu())
+        d_vals_fake.append(d_proba_fake.mean().cpu())
+
+    all_d_losses.append(torch.tensor(d_losses).mean())
+    all_g_losses.append(torch.tensor(g_losses).mean())
+    all_d_real.append(torch.tensor(d_vals_real).mean())
+    all_d_fake.append(torch.tensor(d_vals_fake).mean())
+    print(f'Epoch {epoch:03d} | Avg Losses >>'
+          f' G/D {all_g_losses[-1]:.4f}/{all_d_losses[-1]:.4f}'
+          f' [D-Real: {all_d_real[-1]:.4f}'
+          f' D-Fake: {all_d_fake[-1]:.4f}]')
+    
+    epoch_samples.append(
+        create_samples(gen_model, fixed_z).detach().cpu().numpy()
+    )
+
+import itertools
+fig = plt.figure(figsize=(16, 6))
+## Plotting the losses
+ax = fig.add_subplot(1, 2, 1)
+plt.plot(all_g_losses, label='Generator loss')
+half_d_losses = [all_d_loss/2 for all_d_loss in all_d_losses]
+plt.plot(half_d_losses, label='Discriminator loss')
+plt.legend(fontsize=20)
+ax.set_xlabel('Iteration', size=15)
+ax.set_ylabel('Loss', size=15)
+
+## Plotting the outputs of the discriminator
+ax = fig.add_subplot(1, 2, 2)
+plt.plot(all_d_real, label=r'Real: $D(\mathbf{x})$')
+plt.plot(all_d_fake, label=r'Fake: $D(G(\mathbf{z}))$')
+plt.legend(fontsize=20)
+ax.set_xlabel('Iteration', size=15)
+ax.set_ylabel('Discriminator output', size=15)
+plt.show()
+
+selected_epochs = [1, 2, 4, 10, 50, 100]
+fig = plt.figure(figsize=(10, 14))
+for i,e in enumerate(selected_epochs):
+    for j in range(5):
+        ax = fig.add_subplot(6, 5, i*5+j+1)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        if j == 0:
+            ax.text(
+                -0.06, 0.5, f'Epoch {e}',
+                rotation=90, size=18, color='red',
+                horizontalalignment='right',
+                verticalalignment='center',
+                transform=ax.transAxes
+            )
+
+        image = epoch_samples[e-1][j]
+        ax.imshow(image, cmap='gray_r')
+plt.show()
