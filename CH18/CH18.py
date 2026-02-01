a0f7145e81a4dbc54ab651614b3f36ffc8a66164
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2026-02-01 16:45:28 -0700

    Finished GNN with Pytorch

--- CH18/CH18.py
+++ CH18/CH18.py
@@ -92,6 +92,120 @@ def get_batch_tensor(graph_sizes):
     stops = [starts[idx] + graph_sizes[idx] for idx in range(len(graph_sizes))]
     tot_len = sum(graph_sizes)
     batch_size = len(graph_sizes)
-    #TODO stopped at page 654
-    
+    batch_mat = torch.zeros([batch_size, tot_len]).float()
+    for idx, starts_and_stops in enumerate(zip(starts,stops)):
+        start = starts_and_stops[0]
+        stop = starts_and_stops[1]
+        batch_mat[idx,start:stop] = 1
+    return batch_mat
 
+# batch is a list of dictionaries each containing the representation
+# and label of a graph
+def collate_graphs(batch):
+    adj_mats = [graph['A'] for graph in batch]
+    sizes = [A.size(0) for A in adj_mats]
+    tot_size = sum(sizes)
+    # create batch matrix
+    batch_mat = get_batch_tensor(sizes)
+    # combine feature matrices
+    feat_mats = torch.cat([graph['X'] for graph in batch], dim=0)
+    # combine labels
+    labels = torch.cat([graph['y'] for graph in batch], dim=0)
+    # combine adjacency matrices
+    batch_adj = torch.zeros([tot_size, tot_size],  dtype=torch.float32)
+    accum=0
+    for adj in adj_mats:
+        g_size = adj.shape[0]
+        batch_adj[accum:accum+g_size, accum:accum+g_size] = adj
+        accum =  accum +  g_size
+    repr_and_label = {'A': batch_adj,
+                      'X': feat_mats, 'y': labels,
+                      'batch': batch_mat}
+    return   repr_and_label
+
+def get_graph_dict(G, mapping_dict):
+    # Function  builds dictionary representation of graph G
+    A =  torch.from_numpy(np.asarray(nx.adjacency_matrix(G).todense())).float()
+    # build_graph_color_label_representation()
+    X = torch.from_numpy(
+        build_graph_color_label_representation(G, mapping_dict)).float()
+    # kludge since there is not specific task for this example
+    y =  torch.tensor([[1,0]]).float()
+    return {'A': A, 'X':X, 'y': y, 'batch': None}
+
+# building 4 graphs to treat as a dataset
+blue, orange, green = "#1f77b4", "#ff7f0e", "#2ca02c" 
+mapping_dict = {green:0, blue:1, orange:2}
+G1 =  nx.Graph()
+G1.add_nodes_from([
+    (1,{"color":blue}),
+    (2,{"color":orange}),
+    (3,{"color":blue}),
+    (4,{"color":green})    
+])
+G1.add_edges_from([(1,2), (2,3), (1,3), (3,4)])
+
+G2 =  nx.Graph()
+G2.add_nodes_from([
+    (1,{"color":green}),
+    (2,{"color":green}),
+    (3,{"color":orange}),
+    (4,{"color":orange}),
+    (5,{"color":blue})    
+])
+G2.add_edges_from([(2,3), (3,4), (3,1), (5,1)])
+
+G3 =  nx.Graph()
+G3.add_nodes_from([
+    (1,{"color":orange}),
+    (2,{"color":orange}),
+    (3,{"color":green}),
+    (4,{"color":green}),
+    (5,{"color":blue}),
+    (6,{"color":orange})    
+])
+G3.add_edges_from([(2,3), (3,4), (3,1), (5,1), (2,5), (6,1)])
+
+G4 =  nx.Graph()
+G4.add_nodes_from([
+    (1,{"color":blue}),
+    (2,{"color":blue}),
+    (3,{"color":green})   
+])
+G4.add_edges_from([(1,2), (2,3)])
+
+graph_list = [get_graph_dict(graph, mapping_dict) for  graph in [G1, G2, G3, G4]]
+
+from torch.utils.data import Dataset
+
+class ExampleDataset(Dataset):
+    def __init__(self,graph_list):
+        super().__init__()
+        self.graphs  =  graph_list
+    
+    def  __len__(self):
+        return len(self.graphs)
+    
+    def __getitem__(self, idx):
+        mol_rep  = self.graphs[idx]
+        return mol_rep
+
+from torch.utils.data import DataLoader
+dset= ExampleDataset(graph_list)
+
+loader  =  DataLoader(dset, batch_size=2, shuffle=False, collate_fn=collate_graphs)
+
+node_features = 3
+net  =  NodeNetwork(node_features)
+
+batch_results = []
+for b  in  loader:
+    batch_results.append(net(b['X'], b['A'], b['batch']).detach())
+
+G1_rep = dset[1]
+G1_single = net(G1_rep['X'],  G1_rep['A'], G1_rep['batch']).detach()
+
+G1_batch = batch_results[0][1]
+print(torch.all(torch.isclose(G1_single, G1_batch)))
+
+#Stopped at page 659
